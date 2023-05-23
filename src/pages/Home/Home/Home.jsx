fb3edd968aa94b0eb23729237a4ddb1a28c4e7f0
mominul <ulhouqemomin39@gmail.com>
2023-05-23 17:13:45 +0600

    added banner

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import Banner from '../Carousel/Banner';
+
 const Home = () => {
     return (
         <div>
-        
-    
+       
+    <Banner />
         </div>
     );
 };
